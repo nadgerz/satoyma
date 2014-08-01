af979ddf6ad3588a9d19cce5133122a1c4ac5287
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-08-01 16:35:06 +0200

    typescript?

--- Gruntfile.js
+++ Gruntfile.js
@@ -207,6 +207,7 @@ module.exports = function ( grunt ) {
     grunt.loadNpmTasks( 'grunt-contrib-jasmine' );
     grunt.loadNpmTasks( 'grunt-contrib-jshint' );
     grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
+//    grunt.loadNpmTasks( 'grunt-contrib-typescript' );
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
     grunt.loadNpmTasks( 'grunt-contrib-watch' );
     grunt.loadNpmTasks( 'grunt-htmlhint' );
