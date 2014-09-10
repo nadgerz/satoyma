235dba9f81733888b11fa8bacb06d7a8645ba1a9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-11 00:01:17 +0200

    interpolation

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function(grunt) {
         // so a internal task, like 'server start/stop' can grab these from here
         server: {
             web: {
-                root: "build",
+                root: "#{process.env.SERVER_ROOT || 'build'}",
                 port: 8000
             }
         }
