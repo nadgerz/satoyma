0595cb827ac4f724b3313a2ec730c70e173cebe9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-10 13:14:59 +0200

    think I prefer this format, so we can reference easily elsewhere and seems to follow a pattern - some things like html and less are placeholders

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,48 +11,61 @@ module.exports = function(grunt) {
         // this enables us to use version, name etc in generated files/dirs
         pkg: grunt.file.readJSON('package.json'),
 
-        vendorJs:  [
-            "src/vendor/xxx/xxx.js",
+        // files that our tasks will use
+        files: {
+            html: {
+                src: "index.html"
+            },
 
-            'bower_components//backbone/backbone.js',
-            'bower_components//backbone/index.js',
+            less: {
+                src: ["css/style.less"]
+            },
 
-            'bower_components//bootstrap/dist/js/bootstrap.js',
+            js: {
+                vendor: {
+                    src: [
+                        'bower_components//backbone/backbone.js',
+                        'bower_components//backbone/index.js',
 
-            'bower_components//jquery/dist/jquery.js',
+                        'bower_components//bootstrap/dist/js/bootstrap.js',
 
-            'bower_components//jquery-ui/jquery-ui.js',
-            'bower_components//jquery-ui/ui/i18n/datepicker-de.js',
-            'bower_components//jquery-ui/ui/i18n/datepicker-en-GB.js',
-            'bower_components//jquery-ui/ui/i18n/datepicker-es.js',
-            'bower_components//jquery-ui/ui/i18n/datepicker-fr.js',
-            'bower_components//jquery-ui/ui/i18n/datepicker-no.js',
+                        'bower_components//jquery/dist/jquery.js',
 
-            'bower_components//lodash/dist/lodash.js',
+                        'bower_components//jquery-ui/jquery-ui.js',
+                        'bower_components//jquery-ui/ui/i18n/datepicker-de.js',
+                        'bower_components//jquery-ui/ui/i18n/datepicker-en-GB.js',
+                        'bower_components//jquery-ui/ui/i18n/datepicker-es.js',
+                        'bower_components//jquery-ui/ui/i18n/datepicker-fr.js',
+                        'bower_components//jquery-ui/ui/i18n/datepicker-no.js',
+
+                        'bower_components//lodash/dist/lodash.js',
 //            'bower_components//lodash/dist/lodash.underscore.js',
 
-            'bower_components//moment/benchmarks/clone.js',
-            'bower_components//moment/locale/de-at.js',
-            'bower_components//moment/locale/de.js',
-            'bower_components//moment/locale/en-gb.js',
-            'bower_components//moment/locale/es.js',
-            'bower_components//moment/locale/fr.js',
-            'bower_components//moment/locale/it.js',
-            'bower_components//moment/locale/nl.js',
-            'bower_components//moment/locale/pl.js',
-            'bower_components//moment/locale/pt.js',
-            'bower_components//moment/moment.js',
+                        'bower_components//moment/benchmarks/clone.js',
+                        'bower_components//moment/locale/de-at.js',
+                        'bower_components//moment/locale/de.js',
+                        'bower_components//moment/locale/en-gb.js',
+                        'bower_components//moment/locale/es.js',
+                        'bower_components//moment/locale/fr.js',
+                        'bower_components//moment/locale/it.js',
+                        'bower_components//moment/locale/nl.js',
+                        'bower_components//moment/locale/pl.js',
+                        'bower_components//moment/locale/pt.js',
+                        'bower_components//moment/moment.js',
 
-            'bower_components//react/JSXTransformer.js',
-            'bower_components//react/react-with-addons.js',
-            'bower_components//react/react.js',
+                        'bower_components//react/JSXTransformer.js',
+                        'bower_components//react/react-with-addons.js',
+                        'bower_components//react/react.js',
 
-            'bower_components//requirejs/require.js',
+                        'bower_components//requirejs/require.js',
 
-            'bower_components//underscore/underscore.js',
+                        'bower_components//underscore/underscore.js',
 
-            'dummy.js'
-        ],
+                        'dummy.js'
+                    ],
+                }
+            }
+        },
 
         concat: {
             options: {
@@ -68,7 +81,7 @@ module.exports = function(grunt) {
             app: {
                 // the files to concatenate
                 src: [
-                    "<%= vendorJs %>",
+                    "<%= files.js.vendor.src %>",
                     "src/js/app.js"
                 ],
 
@@ -82,6 +95,27 @@ module.exports = function(grunt) {
 
 
         watch: {
+            options: {
+                // need chrome livereload extension
+                // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en
+                livereload: true
+            },
+
+            html: {
+                files: ["<%= files.html.src %>"],
+                tasks: ["copy"]
+            },
+
+            js: {
+                files: ["<%= files.js.vendor.src %>"],
+                tasks: ["concat"]
+            },
+
+            less: {
+                files: ["<%= files.less.src %>"],
+                tasks: ["less:dev"]
+            },
+
             app: {
                 // SELF REFERENTIAL! COOL!
                 files: ["<%= concat.app.src %>"],
@@ -90,23 +124,23 @@ module.exports = function(grunt) {
         },
 
 
-	copy: {
-	  main: {
-	    files: [
-	      // includes files within path
-//	      {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
+        copy: {
+          main: {
+            files: [
+              // includes files within path
+//              {expand: true, src: ['path/*'], dest: 'dest/', filter: 'isFile'},
 
-	      // includes files within path and its sub-directories
-//	      {expand: true, src: ['path/**'], dest: 'dest/'},
+              // includes files within path and its sub-directories
+//              {expand: true, src: ['path/**'], dest: 'dest/'},
 
-	      // makes all src relative to cwd
-//	      {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+              // makes all src relative to cwd
+//              {expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
 
-	      // flattens results to a single level
-//	      {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'}
-	    ]
-	  }
-	}
+              // flattens results to a single level
+//              {expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'}
+            ]
+          }
+        }
     });
 
 
