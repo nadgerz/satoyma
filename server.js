aaed8ef64665c3b7472b0a77552331e69ee9a1fd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 16:48:07 +0200

    paths are hurting me at the moment, so to get this to work I am bringing this into a simple structure, which I will delete, once I have it all working as they should - these are taking off from where we left off in src/js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '../../bower_components'));
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+var port = process.env.PORT || 3000;
+var ip   = process.env.IP   || "localhost";
+
+app.listen(port, ip);
+
+console.log('Please open http://' + ip + ':' + port + '/');
+
