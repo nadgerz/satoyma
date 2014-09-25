16efe658923a7e832ad88c9c54efa80d4ed961a8
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 19:56:44 +0200

    failed because jobsList not defined

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,7 +1,7 @@
 describe("get jobs", function() {
 //    jobsList = [];
     it("should never be empty since jobs are seeded", function() {
-//        expect(jobsList.length).to.be.at.least(1);
+        expect(jobsList.length).to.be.at.least(1);
     });
 });
 
