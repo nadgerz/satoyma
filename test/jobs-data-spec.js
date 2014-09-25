365c821abafb514c251af40c9618e31621918d4e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 20:25:46 +0200

    Fixed:
      1) get jobs should never be empty since jobs are seeded:
           Error: timeout of 2000ms exceeded

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -8,6 +8,7 @@ describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         mongoose.model('Job').find({}).exec(function(error, jobsList) {
             expect(jobsList.length).to.be.at.least(1);
+            done();
         })
     });
 });
