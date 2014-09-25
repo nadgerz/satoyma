5af0260fadab7d568d6b2c0153a43de7240ebf3f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 20:03:32 +0200

    now try and get the jobs from the db - bring across the connection and the get-jobs call.  Instead of res.send, we use the expect line

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,9 +1,13 @@
 var expect = require("chai").expect;
 
+mongoose.connect('mongodb://dms:nrg@ds039850.mongolab.com:39850/jobfinder');
+
 describe("get jobs", function() {
     jobsList = [];
     it("should never be empty since jobs are seeded", function() {
-        expect(jobsList.length).to.be.at.least(1);
+        mongoose.model('Job').find({}).exec(function(error, collection) {
+            expect(jobsList.length).to.be.at.least(1);
+        })
     });
 });
 
