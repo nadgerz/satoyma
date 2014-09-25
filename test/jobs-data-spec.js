7d2463a5c473847d03b84e43a4bd0760de1634ea
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 20:05:31 +0200

    rename collection to jobsList as this makes more sense for this test

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -4,9 +4,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://dms:nrg@ds039850.mongolab.com:39850/jobfinder');
 
 describe("get jobs", function() {
-    jobsList = [];
     it("should never be empty since jobs are seeded", function() {
-        mongoose.model('Job').find({}).exec(function(error, collection) {
+        mongoose.model('Job').find({}).exec(function(error, jobsList) {
             expect(jobsList.length).to.be.at.least(1);
         })
     });
