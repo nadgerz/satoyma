f6a40bd92478e1b167c1f0986ba51cd47d4fba3f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 20:41:42 +0200

    to test seedJobs, we need to empty out the collection each time... here is the drop... which on it's own will cause the test to fail

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -2,13 +2,18 @@ var expect = require("chai").expect;
 var mongoose = require('mongoose');
 var jobModel = require('../models/Job');
 
+function resetJobs(callback) {
+    mongoose.connection.collections['jobs'].drop(callback);
+};
 
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         mongoose.connect('mongodb://dms:nrg@ds039850.mongolab.com:39850/jobfinder', function() {
-            mongoose.model('Job').find({}).exec(function(error, jobsList) {
-                expect(jobsList.length).to.be.at.least(1);
-                done();
+            resetJobs(function() {
+                mongoose.model('Job').find({}).exec(function(error, jobsList) {
+                    expect(jobsList.length).to.be.at.least(1);
+                    done();
+                });
             });
         });
     });
