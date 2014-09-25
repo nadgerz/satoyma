99d8806aea678560776b4ac4697eac8dafd152e9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-25 20:10:59 +0200

    w/o the done, the expect is never called, so this looked like a passing test, but it was never being evaulated. Now with the done param it is trying but will timeout after 2 seconds

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -5,7 +5,7 @@ var jobModel = require('../models/Job');
 mongoose.connect('mongodb://dms:nrg@ds039850.mongolab.com:39850/jobfinder');
 
 describe("get jobs", function() {
-    it("should never be empty since jobs are seeded", function() {
+    it("should never be empty since jobs are seeded", function(done) {
         mongoose.model('Job').find({}).exec(function(error, jobsList) {
             expect(jobsList.length).to.be.at.least(1);
         })
